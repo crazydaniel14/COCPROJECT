e90ffd7a5545b4c794073e82fbd3ff6146d366de
crazydaniel14 <santiago.dhv@gmail.com>
2026-03-12 17:11:51 -0400

    script image map

--- script.js
+++ script.js
@@ -75,6 +75,9 @@ const IMAGE_MAP = {
   "Spell Tower": ["Lvl 4"],
   "Inferno Tower": ["Lvl 12"],
   "X-Bow": ["Lvl 12&13&14"]
+  "Hero Hall":["Lvl 12]"]
+  "Laboratory":["Lvl 16"]
+  "Clan Castle":["Lvl 14"]
 };
 
 function getUpgradeImage(upgradeName) {
